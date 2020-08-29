74ecf7374908c258db99dbbb121cff37f40c3995
Umar Farooq <umarfoo@outlook.com>
2020-08-29 00:59:29 -0500

    Working DropDown

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-function buildPlot() {
+// Setting function on dropdownmenu to print new graph based on option selected
+d3.selectAll("#selDataset").on("change", selection)
+
+// Printing the first option in the dropdownmenu
+buildPlot(940);
+
+// create function to make dropdown value
+    // Select the dropdownmenu with d3
+d3.json("data/samples.json").then(function(dataOtu){
+    
+    // get names list from json data
+    var names_list = dataOtu.names;
+    
+     // make for loop to append/push new options to dropdown menu using d3
+    var dropDownMenu = d3.select("#selDataset");
+    dropDownMenu.selectAll("option").data(names_list).enter().append("option").text(function(d) {
+        return d;
+         })
+           .attr("value", function(d) {
+        return d;
+        });
+});
+
+function selection(){
+    // Selecting Dropdownmenu
+    var dropDownMenu = d3.select("#selDataset")
+    // Selecting Value of the dropdown which has been selected 
+    var input = dropDownMenu.node().value;
+    // running plot function with new node value
+    buildPlot(input)
+};
+
+function buildPlot(input) {
     d3.json("data/samples.json").then(function(dataOtu){
         // Make a list of all the data in names
         var names_list = dataOtu.names;
         
         // Input sample ID selected from HTML or USER
-        var user_input = '940';
+        var user_input = input;
         
         // fetch the index value based on the name input by user
         var sample_id = names_list.findIndex(i => i == user_input);
@@ -51,9 +83,20 @@ function buildPlot() {
         };
         Plotly.newPlot('bubble', data, layout);
 
-        
+        // Removing all data in metadata box before printing new data
+        d3.select("#sample-metadata").selectAll("p").remove();
+        // Fetch Metadata based on userinput
+        var meta_text = dataOtu.metadata[sample_id];
 
+        // Cereating a list of object in the User MetaData
+        var m_keys = Object.entries(meta_text);
+
+        // Selecting MetaData HTML for data input
+        var meta_data = d3.select("#sample-metadata");
+
+        // Creating loop to input data in Demographic Info
+        m_keys.forEach(function(m_data){
+        meta_data.append("p").text(`${m_data[0]}: ${m_data[1]}`)
+        });
     }); 
 };
-
-buildPlot();
